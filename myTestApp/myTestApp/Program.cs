c2d778eff628f5565d3b9beaca841a6f31ff5065
Sisir Patro <79246986+sispatro@users.noreply.github.com>
2022-06-09 17:04:20 +0530

    test app addition
    test app addition

---
+++ myTestApp/myTestApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for(int i = 0; i < 1000; i++)
+{
+    Console.WriteLine(" Looped as " + i.ToString());
+    Thread.Sleep(100);
+}
+Console.WriteLine("Good Bye!");
